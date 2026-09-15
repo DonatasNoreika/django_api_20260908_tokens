d0cbd2ffa18b063acdb6c83998c935c45c6aa0e2
DonatasNoreika <donoras@gmail.com>
2026-09-15 18:42:09 +0300

    4 +nustatytas token autorizavimas

--- api_example/api_example/settings.py
+++ api_example/api_example/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'postit_api',
     'rest_framework',
+    'rest_framework.authtoken',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -131,3 +132,9 @@ MAILERS = {
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'postit_api/media')
 MEDIA_URL = '/media/'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+}
